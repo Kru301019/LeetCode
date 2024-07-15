345cb61eabfe07b3aadba4dae6552f37bec45bbd
Kru301019 <krujoshi3010@gmail.com>
2024-07-15 15:23:46 +0530

    ++++

--- 2.hashmap/219. Contains Duplicate II.py
+++ 2.hashmap/219. Contains Duplicate II.py
@@ -1,19 +1,27 @@
 from collections import Counter
 
+
+def getDuplicateI(myDict, num):
+    for key, value in myDict.items():
+        if key == num:
+            return value
+
 def Contains_Duplicate_II(nums, k):
-    countDup = Counter(nums)
-    dupNum = []
-    for key, value in countDup.items():
-        if value >= 2:
-            dupNum.append(key)
-    j=0
-    while j != len(dupNum):
-        for i,num in enumerate(nums):
-            if num == dupNum[j]:
-                print(j,i)
-        j+=1
+    myDict = {}
 
+    for i,num in enumerate(nums):
+        if not num in myDict: 
+            myDict[num] = i
+        else:
+            result = i - getDuplicateI(myDict, num)
+            if result == k:
+                return True
+            else:
+                myDict[num] = i
+    
+    return False
+            
 
-nums = [1, 2, 3, 1, 2, 3]
+nums = [1, 2, 3, 1]
 k = 3
-Contains_Duplicate_II(nums, k)
+print(Contains_Duplicate_II(nums, k))
