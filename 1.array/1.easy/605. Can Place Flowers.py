31d5c39f901fdfef1a54b35b25a3f88cc7b2686c
Kru301019 <krujoshi3010@gmail.com>
2024-08-20 12:14:38 +0530

    ++

--- 1.array/1.easy/605. Can Place Flowers.py
+++ 1.array/1.easy/605. Can Place Flowers.py
@@ -1,18 +1,28 @@
-def checkEmpty(flowerbed, n):
-    
-    
-
+def checkEmpty(flowerbed,i):
+    if i == 0 :
+        return flowerbed[i+1] == 0
+    elif i == len(flowerbed) - 1:
+        return flowerbed[i-1] == 0
+    else:
+        return flowerbed[i-1] == 0 and flowerbed[i+1] ==0
+        
+      
 def Can_Place_Flowers(flowerbed, n):
     
     count = 0
-    
+
     for i in range(len(flowerbed)):
         
         if flowerbed[i] == 0:
-            prevEmpty = checkEmpty(flowerbed, n)
-            nextEmpty = checkEmpty(flowerbed, n)
-        
-
-flowerbed = [ 1, 0, 1, 0]
+            ifEmpty = checkEmpty(flowerbed,i)
+           
+            if ifEmpty:
+                flowerbed[i] = 1
+                count+=1
+                if count >= n:
+                    return True
+    return  count >= n
+    
+flowerbed = [ 1, 0, 0, 0, 1]
 n = 1
 Can_Place_Flowers(flowerbed, n)
