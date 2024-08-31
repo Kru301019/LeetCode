108c7b5d7566ad3690e0caca8170b135192583c2
Kru301019 <krujoshi3010@gmail.com>
2024-08-31 11:40:06 +0530

    ++

---
+++ 8.matrix/1.easy#/1582. Special Positions in a Binary Matrix.py
@@ -0,0 +1,18 @@
+def Special_Positions_Matrix(mat):
+    n = len(mat)
+    
+    print
+mat =  [
+        [1, 0, 0], 
+        [0, 0, 1], 
+        [1, 0, 0]
+                 ]
+
+Special_Positions_Matrix(mat)
+'''
+00 01 02
+00 10 12
+
+12 02 03
+12 11 10
+'''
