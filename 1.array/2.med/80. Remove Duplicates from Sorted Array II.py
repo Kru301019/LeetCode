57d907f6c47440f3ad4a05e5925da32a0c609e20
Kru301019 <krujoshi3010@gmail.com>
2024-08-31 17:41:08 +0530

    ++

--- 1.array/2.med/80. Remove Duplicates from Sorted Array II.py
+++ 1.array/2.med/80. Remove Duplicates from Sorted Array II.py
@@ -1,9 +1,14 @@
 def Remove_Duplicates_from_Sorted_Array_II(nums):
-	
-	nums[4] = nums[6]
-	nums[5] = nums[7] 
-	
+	right = len(nums)-1
+	for i in range(len(nums)-2):
+		if [i] ==nums[i+2]:
+      		nums[i+2] = nums[right]
+		right-=1
 	
 
 nums = [0,0,1,1,1,1,2,3,3]
 Remove_Duplicates_from_Sorted_Array_II(nums)
+'''	nums[4] = nums[6]
+	nums[5] = nums[7] 
+	nums[6] = nums[8]
+'''
