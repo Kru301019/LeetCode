4049a4eb27cda7b25e7a1933375ea3c2d9947d33
Kru301019 <krujoshi3010@gmail.com>
2024-08-31 17:06:08 +0530

    ++

---
+++ 1.array/2.med/80. Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,7 @@
+def Remove_Duplicates_from_Sorted_Array_II(nums):
+	
+	for i in range(len(nums)):
+		print(i)
+
+nums = [0,0,1,1,1,1,2,3,3]
+Remove_Duplicates_from_Sorted_Array_II(nums)
--- 8.matrix/1.easy#/1582. Special Positions in a Binary Matrix.py
+++ 8.matrix/1.easy#/1582. Special Positions in a Binary Matrix.py
@@ -1,13 +1,28 @@
 def Special_Positions_Matrix(mat):
-    n = len(mat)
+    m = len(mat)
+    n = len(mat[0])
+    rowCount = [0] * m
+    colCount = [0] * n
     
-    print
-mat =  [
-        [1, 0, 0], 
-        [0, 0, 1], 
-        [1, 0, 0]
-                 ]
+    for i in range(m):
+        for j in range(n):
+            if mat[i][j] == 1:
+                rowCount[i]+=1
+                colCount[j]+=1
+            
+    specialCount = 0
+    print(rowCount, colCount)
+    for i in range(m):
+        for j in range(n):
+            if mat[i][j] == 1 and rowCount[i] == 1 and colCount[j] == 1:
+                print(i,j)
 
+
+mat =  [
+             [1, 0, 0, 0], 
+             [0, 0, 1, 0], 
+             [1, 0, 0, 1]
+             ]
 Special_Positions_Matrix(mat)
 '''
 00 01 02
