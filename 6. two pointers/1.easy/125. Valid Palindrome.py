90f607260ad17f3e1473e8eac07c130ddd42e112
Kru301019 <krujoshi3010@gmail.com>
2024-09-15 12:51:29 +0530

    ++

---
+++ 6. two pointers/1.easy/125. Valid Palindrome.py
@@ -0,0 +1,4 @@
+def Valid_Palindrome():
+    pass
+
+
--- 6. two pointers/2.med/167. Two Sum II - Input Array Is Sorted.py
+++ 6. two pointers/2.med/167. Two Sum II - Input Array Is Sorted.py
@@ -1,2 +1,17 @@
-def Input_Array_Is_Sorted():
-    pass
+def Input_Array_Is_Sorted(numbers, target):
+    left, right = 0, len(numbers) - 1
+    
+    while left<right:
+        
+        curruntSum = numbers[left] + numbers[right]
+        
+        if curruntSum == target:
+            return [left+1, right+1]
+        elif curruntSum < target:
+            left+=1
+        else:
+            right-=1
+
+numbers = [2, 7, 11, 15]
+target = 18
+Input_Array_Is_Sorted(numbers, target)
