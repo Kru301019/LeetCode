822cbf791e014911c2cffb4841d4abe51c29dda2
Kru301019 <krujoshi3010@gmail.com>
2025-02-02 11:41:29 +0000

    ++

--- sql/1.easy/ 197. Rising Temperature.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Weather_table( id serial primary key, recordDate date unique, temperature int );
-INSERT INTO Weather_table (recordDate, temperature) values ('2015-01-01' , 10), ('2015-01-02' , 25), ('2015-01-03' , 20), ('2015-01-04' , 30);
-
-SELECT  p.id
-FROM Weather_table AS c
-JOIN Weather_table AS p
-ON c.recordDate = p.recordDate - interval '1 day'
-WHERE p.temperature > c.temperature;
