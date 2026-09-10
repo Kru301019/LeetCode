f08e717313ccc63e69503cdc1a2ca6d436f719e0
Kru301019 <krujoshi3010@gmail.com>
2026-09-10 19:37:07 +0530

    +

--- Data eng/1.Easy/Everything Said Twice.py
+++ Data eng/1.Easy/Everything Said Twice.py
@@ -1,29 +1,30 @@
-def input(text):
+def word_frequency(text):
+  count_word = []
+  tempo = ""
 
-    count_word = []
-    tempo =''
-    for word in text:
-        if word != ' ':
-            tempo += word
+  for char in text:
+    if char != " ":
+      tempo += char
+    else:
+      if tempo:  # Appends word when a space is found (and ignores empty spaces)
+        count_word.append(tempo)
+        tempo = ""
 
-        if word == ' ':
-            count_word.append(tempo)
-            tempo =''
+  # FIX: Catch the last word remaining in tempo after the loop ends
+  if tempo:
+    count_word.append(tempo)
 
-    my_dict = {}
-    
-    
-    for arr in count_word:
+  my_dict = {}
 
-    
-        if arr  in my_dict:
-            
-            my_dict[arr] += 1
+  for word in count_word:
+    if word in my_dict:
+      my_dict[word] += 1
+    else:
+      my_dict[word] = 1
 
-        else:
-            my_dict[arr] = 1
+  return my_dict
 
-    return my_dict
 
-text ="click view click submit view view"
-input(text)
+# Test run
+text = "click view click submit view view"
+print(word_frequency(text))
